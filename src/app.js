86c83142050b2db3f9599683354df25dba6cfa1b
ymontesv <yarmain.m@gmail.com>
2021-03-07 19:26:27 +0000

    Random Card

--- src/app.js
+++ src/app.js
@@ -6,6 +6,39 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let numero = document.querySelector("p");
+
+  let palosposibles = ["suitspades", "suitclubs", "suithearts", "suitdiamonds"];
+
+  let randomNum1 = Math.floor(Math.random() * 4);
+
+  document.getElementById("card").classList.add(palosposibles[randomNum1]);
+
+  if (
+    palosposibles[randomNum1] === "suithearts" ||
+    palosposibles[randomNum1] === "suitdiamonds"
+  ) {
+    numero.style.color = "red";
+  }
+
+  let numerosPosibles = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let randomNum2 = Math.floor(Math.random() * 14);
+
+  numero.textContent = numerosPosibles[randomNum2];
 };
